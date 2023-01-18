9a4946597476b39dd7492c4a7db0ca29eaea18ac
Bhazi <109984736+Bhazi@users.noreply.github.com>
2023-01-18 12:13:28 +0530

    multiple request activated

--- first-app.js
+++ first-app.js
@@ -1,12 +1,28 @@
 var http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>From server</title></head>");
-  res.write("<body><h1>Basim</h1>");
-  res.write("</html>");
-  res.end();
+  const url = req.url;
+  console.log(url);
+  if (url == "/home") {
+    res.write("<html>");
+    res.write("<head><title>From server</title></head>");
+    res.write("<body><h1>Welcome home</h1>");
+    res.write("</html>");
+    res.end();
+  } else if (url == "/about") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>From server</title></head>");
+    res.write("<body><h1>Welcome to About Us page</h1>");
+    res.write("</html>");
+    res.end();
+  } else if (url == "/node") {
+    res.write("<html>");
+    res.write("<head><title>From server</title></head>");
+    res.write("<body><h1>Welcome to my Node Js project</h1>");
+    res.write("</html>");
+    res.end();
+  }
 });
 
 server.listen(4000);
