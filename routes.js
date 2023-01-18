6d79a0066e61bb3bedcd51fd0ebe7fad8cd9c267
Bhazi <109984736+Bhazi@users.noreply.github.com>
2023-01-18 20:08:20 +0530

    added with routes file, and export it.

---
+++ routes.js
@@ -0,0 +1,47 @@
+var fs = require("fs");
+
+var value;
+fs.readFile("message.txt", "utf8", function (err, data) {
+  value = data;
+});
+
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url == "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter message</title></head>");
+    res.write(`<p>${value}</p>`);
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+      value = message;
+    });
+
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>From server</title></head>");
+  res.write("<body><h1>Welcome to my Node Js project</h1>");
+  res.write("</html>");
+  res.end();
+};
+
+module.exports = requestHandler;
