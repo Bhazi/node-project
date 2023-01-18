f29b7739bf5c5b173c8078195f84a915f347c1e7
Bhazi <109984736+Bhazi@users.noreply.github.com>
2023-01-18 11:35:01 +0530

    name printing with help of calling of server

--- first-app.js
+++ first-app.js
@@ -1,3 +1,12 @@
-var fs = require("fs");
+var http = require("http");
 
-fs.writeFileSync("hello.txt", "hello world");
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>From server</title></head>");
+  res.write("<body><h1>Basim</h1>");
+  res.write("</html>");
+  res.end();
+});
+
+server.listen(4000);
