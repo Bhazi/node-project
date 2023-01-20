713e8274e8db0a34de99077ec92cac485a8d4084
Bhazi <109984736+Bhazi@users.noreply.github.com>
2023-01-20 10:58:39 +0530

    shop site with contact form

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,14 @@
 const express = require("express");
+const path = require("path");
 
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/admin/add-product" method="POST"><label>Name </label><input type="text" name="title"></input><label> Size </label><input type="number" name="size"></input><button type="submit">Add product</button></form>'
-  );
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
-  const obj = JSON.parse(JSON.stringify(req.body));
-  console.log(obj);
+  console.log(req.body);
   res.redirect("/");
 });
 
--- routes/success.js
+++ routes/success.js
@@ -3,8 +3,8 @@ const path = require("path");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+router.post("/success", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "../", "views", "success.html"));
 });
 
 module.exports = router;
