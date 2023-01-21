7d6f2523817c22ca9b560b7ac51d3909957685b8
Bhazi <109984736+Bhazi@users.noreply.github.com>
2023-01-21 15:03:11 +0530

    shop project with dynamic routes (product details)

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,18 @@
-const express = require("express");
-const adminUsController = require("../controller/admin-controller");
+const path = require('path');
+
+const express = require('express');
+
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-router.get("/add-product", adminUsController.getAddProduct);
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
 
-router.post("/add-product", adminUsController.postAddproduct);
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
+
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
