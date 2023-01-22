021a552b52ca354333833d031495f8a09fd73cfb
Bhazi <109984736+Bhazi@users.noreply.github.com>
2023-01-22 17:47:34 +0530

    added cart in models

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const path = require("path");
 
-const app = express();
+// const app = express();
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-const contactusRoutes = require("./routes/contact-us");
-const successRoutes = require("./routes/success");
-const errorRoutes = require("./controller/404");
-const productPageRoutes = require("./routes/productPage");
-const productDetailsutes = require("./routes/productDetails");
+// const adminRoutes = require("./routes/admin");
+// const shopRoutes = require("./routes/shop");
+// const contactusRoutes = require("./routes/contact-us");
+// const successRoutes = require("./routes/success");
+// const errorRoutes = require("./controller/404");
+// const productPageRoutes = require("./routes/productPage");
+// const productDetailsutes = require("./routes/productDetails");
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/admin", adminRoutes);
-app.use(shopRoutes);
-app.use(contactusRoutes);
-app.use(successRoutes);
-app.use(productPageRoutes);
-app.use(productDetailsutes);
+// app.use("/admin", adminRoutes);
+// app.use(shopRoutes);
+// app.use(contactusRoutes);
+// app.use(successRoutes);
+// app.use(productPageRoutes);
+// app.use(productDetailsutes);
 
-app.use(errorRoutes.getError);
+// app.use(errorRoutes.getError);
 
-app.listen(3000);
+// app.listen(3000);
+
+
