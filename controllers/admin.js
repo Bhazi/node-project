c17dcfdde07ec704989bb60861e6cd61aaadbcc6
Bhazi <109984736+Bhazi@users.noreply.github.com>
2023-01-25 02:09:51 +0530

    update with sequelize node package manager

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,10 +13,13 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null, title, imageUrl, description, price);
-  product
-    .save()
-    .then(res.redirect("/"))
+  Product.create({
+    title: title,
+    price: price,
+    imageUrl: imageUrl,
+    description: description,
+  })
+    .then((data) => console.log(data))
     .catch((err) => console.log(err));
 };
 
@@ -26,7 +29,7 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect("/");
   }
   const prodId = req.params.productId;
-  Product.findById(prodId, (product) => {
+  Product.findByPk(prodId).then((product) => {
     if (!product) {
       return res.redirect("/");
     }
@@ -45,28 +48,37 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedPrice = req.body.price;
   const updatedDescription = req.body.description;
-  const updatedProduct = new Product(
-    prodId,
-    updatedTitle,
-    updatedImageUrl,
-    updatedDescription,
-    updatedPrice
-  );
-  updatedProduct.save();
+  Product.findByPk(prodId)
+    .then((product) => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.description = updatedDescription;
+      product.imageUrl = updatedImageUrl;
+      return product.save();
+    })
+    .then((result) => {
+      console.log("UPDATED PRODUCT");
+    })
+    .catch((err) => console.log(err));
   res.redirect("/admin/products");
 };
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.delete(prodId);
+  console.log(prodId);
+  Product.destroy({ where: {id : prodId} })
+    .then((result) => {
+      console.log("deleted successfully");
+    })
+    .catch((err) => console.log(err));
   res.redirect("/admin/products");
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
+  Product.findAll()
+    .then((data) => {
       res.render("admin/products", {
-        prods: rows,
+        prods: data,
         pageTitle: "Admin Products",
         path: "/admin/products",
       });
--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2')
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'node-complete',
-    password:'limitiedidu@4.7'
-})
+const sequelize = new Sequelize("node-complete", "root", "limitiedidu@4.7", {
+  dialect: "mysql",
+  host: "localhost",
+});
 
-module.exports = pool.promise();
+module.exports = sequelize;
